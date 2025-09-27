3721a4499a79ccdec89b7d2f584d2f3a8029faa3
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-09-27 12:42:30 +0300

    Implement controlled booking form

--- plane-ticket-booking/src/components/BookingForm/BookingForm.jsx
+++ plane-ticket-booking/src/components/BookingForm/BookingForm.jsx
@@ -1,45 +1,108 @@
+import { useState } from 'react';
 import './BookingForm.scss';
 
 export default function BookingForm() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        departureAirportId: '',
+        arrivalAirportId: '',
+        departureDate: '',
+        returnDate: '',
+    });
+
+    const handleChange = (e) => {
+        console.log(e.target);
+        console.log(e.currentTarget);
+
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        
+    };
+
     return (
         <section className="booking-form-container">
             <h2>Book a Flight</h2>
 
-            <form action="/" id='booking' className='booking-form'>
+            <form action="/" id='booking' className='booking-form' onSubmit={handleSubmit}>
                 {/* First name */}
                 <div className="form-row">
                     <div className="form-group">
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text" id='firstName' name='firstName' />
+                        <input
+                            type="text"
+                            id='firstName'
+                            name='firstName'
+                            value={formData.firstName}
+                            onChange={handleChange}
+                        />
                     </div>
-                {/* Last name */}
+                    {/* Last name */}
                     <div className="form-group">
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" id='lastName' name='lastName' />
+                        <input
+                            type="text"
+                            id='lastName'
+                            name='lastName'
+                            value={formData.lastName}
+                            onChange={handleChange}
+                        />
                     </div>
                 </div>
                 {/* Departure Airport */}
                 <div className="form-row">
                     <div className="form-group">
                         <label htmlFor="departureAirportId">Departure Airport</label>
-                        <input type="text" id='departureAirportId' name='departureAirportId' />
+                        <input
+                            type="text"
+                            id='departureAirportId'
+                            name='departureAirportId'
+                            value={formData.departureAirportId}
+                            onChange={handleChange}
+                        />
                     </div>
-                {/* Arrival Airport */}
+                    {/* Arrival Airport */}
                     <div className="form-group">
                         <label htmlFor="arrivalAirportId">Arrival Airport</label>
-                        <input type="text" id='arrivalAirportId' name='arrivalAirportId' />
+                        <input
+                            type="text"
+                            id='arrivalAirportId'
+                            name='arrivalAirportId'
+                            value={formData.arrivalAirportId}
+                            onChange={handleChange}
+                        />
                     </div>
                 </div>
                 {/* Date of departure */}
                 <div className="form-row">
                     <div className="form-group">
                         <label htmlFor="departureDate">Departure Date</label>
-                        <input type="date" id='departureDate' name='departureDate' />
+                        <input
+                            type="date"
+                            id='departureDate'
+                            name='departureDate'
+                            value={formData.departureDate}
+                            onChange={handleChange}
+                        />
                     </div>
-                {/* Date of return */}
+                    {/* Date of return */}
                     <div className="form-group">
                         <label htmlFor="returnDate">Return Date</label>
-                        <input type="date" id='returnDate' name='returnDate' />
+                        <input
+                            type="date"
+                            id='returnDate'
+                            name='returnDate'
+                            value={formData.returnDate}
+                            onChange={handleChange}
+                        />
                     </div>
                 </div>
 
